ba3c072f343f98b8d79b8e24ccbede39d51ddc47
kenshouse <hausukenzu0@gmail.com>
2020-06-03 21:49:55 +0900

    Lesson7　課題
    非常に見づらいですが、確認よろしくお願いします

---
+++ Assets/Scripts/Cubescript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubescript : MonoBehaviour {
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    public AudioClip block;
+    AudioSource audioSource;
+
+
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+        if (transform.position.y == 0)
+        {
+            audioSource.PlayOneShot(block);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Cube")
+        {
+            audioSource.PlayOneShot(block);
+        }
+        if (collision.gameObject.tag=="Ground" )
+        {
+            audioSource.PlayOneShot(block);
+        }
+    }
+
+
+
+}
